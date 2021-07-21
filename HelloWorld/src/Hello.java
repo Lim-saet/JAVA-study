9d1d1f46c85cbb9d75b637ee9072fdb56eb89f43
ehxhf <ehxhf@DESKTOP-NM7CIT9>
2021-07-21 18:44:33 +0900

    깃연동 커밋

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//문자열 변수 하나 선언하고, 자기 이름 영문을 변수에 저장한 후 그 변수 안의 문자열을 출력한다.
+		String name="Lim SaetByeol";
+		System.out.println(name);
+	     
+	}
+}
