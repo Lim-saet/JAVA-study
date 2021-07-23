cecfaed397d2b6a22f3c27ef2d729d8bfda9c981
ehxhf <ehxhf@DESKTOP-NM7CIT9>
2021-07-23 09:52:08 +0900

    1교시 커밋

---
+++ HelloWorld/src/claseEn23deJulio/Tarea01.java
@@ -0,0 +1,14 @@
+package claseEn23deJulio;
+
+import java.util.Scanner;
+
+public class Tarea01 {
+
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 String str=s.nextLine();
+		 int n=Integer.parseInt(str);
+
+	}
+
+}
