9ddf64f5ed5c633a2ee4c3d8a5e3c623ceb28bef
matsk311 <matsk311@naver.com>
2021-07-23 10:58:23 +0900

    2교시 커밋

--- HelloWorld/src/claseEn23deJulio/Tarea01.java
+++ HelloWorld/src/claseEn23deJulio/Tarea01.java
@@ -8,7 +8,18 @@ public class Tarea01 {
 		 Scanner s=new Scanner(System.in);
 		 String str=s.nextLine();
 		 int n=Integer.parseInt(str);
-
+		 if (n>89){
+			 System.out.println("A");
+		 } else if (n>79) {
+			 System.out.println("B");
+		 } else if (n>69) {
+			 System.out.println("C");
+		 } else if (n>59) {
+			 System.out.println("D");
+		 } else {
+			 System.out.println("F");
+		 }
+		 s.close();
 	}
 
 }
