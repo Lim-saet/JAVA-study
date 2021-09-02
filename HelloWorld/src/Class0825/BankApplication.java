231517432b8cf3898b731a18c33072ea87fe82e6
matsk311 <matsk311@naver.com>
2021-09-02 19:51:29 +0900

    은행프로그램

--- HelloWorld/src/Class0825/BankApplication.java
+++ HelloWorld/src/Class0825/BankApplication.java
@@ -5,71 +5,124 @@ import java.util.Scanner;
 public class BankApplication {
 	private static Account[] accountArray = new Account[100];
 	private static Scanner scanner = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		boolean run = true;		
-		while(run) {
-			System.out.println("—————————————————————————————");
-			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
-			System.out.println("—————————————————————————————");
+		boolean run = true;
+		while (run) {
+			System.out.println("------------------------------------------------");
+			System.out.println("1. 계좌생성 | 2. 계좌목록 | 3. 예금 | 4. 출금 | 5. 종료");
+			System.out.println("------------------------------------------------");
 			System.out.print("선택> ");
-			
+
+			//int selectNo = Integer.parseInt(scanner.nextLine());
 			int selectNo = scanner.nextInt();
-			
-			if(selectNo == 1) {
+
+			if (selectNo == 1) {
 				createAccount();
-			} else if(selectNo == 2) {
+			} else if (selectNo == 2) {
 				accountList();
-			} else if(selectNo == 3) {
+			} else if (selectNo == 3) {
 				deposit();
-			} else if(selectNo == 4) {
+			} else if (selectNo == 4) {
 				withdraw();
-			} else if(selectNo == 5) {
+			} else if (selectNo == 5) {
 				run = false;
 			}
 		}
 		System.out.println("프로그램 종료");
 	}
-	
-	//계좌생성하기
+
 	private static void createAccount() {
-		System.out.println("------------");
+		System.out.println("--------------");
 		System.out.println("계좌생성");
-		System.out.println("------------");
-		String ano= scanner.nextLine();
-		System.out.println("계좌주");
-		String owner=scanner.nextLine();
-		System.out.println("초기입금액");
+		System.out.println("--------------");
+		System.out.print("계좌번호: ");
+		String ano = scanner.next();
+		System.out.print("계좌주: ");
+		String owner = scanner.next();
+		System.out.print("초기입금액: ");
 		int balance = scanner.nextInt();
-		System.out.println("계좌가 생성되었습니다.");
-		Account account=new Account(ano,owner,balance);
-		
+
+		Account account = new Account(ano, owner, balance);
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] == null) {
+				accountArray[i] = account;
+				System.out.println("결과: 계좌가 생성되었습니다.");
+				break;
+			}
+		}
+
 	}
-	
-	//계좌목록보기
+
 	private static void accountList() {
-		//for문으로 빈배열의 자리를 찾아 계좌정보를 넣음 
-		for(int i=0;i<accountArray.length;i++){
-			if(accountArray[i]==null) {
-				String ch_balance = Integer.toString(balance);
-				accountArray = new String[i] {ano,owner,ch_balance};
+		System.out.println("--------------");
+		System.out.println("계좌목록");
+		System.out.println("--------------");
+
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				System.out.println(accountArray[i].getAno() + "\t" + accountArray[i].getOwner() + "\t"
+						+ accountArray[i].getBalance());
 			}
 		}
 	}
-	
-	//예금하기
+
 	private static void deposit() {
+
+		System.out.println("--------------");
+		System.out.println("예금");
+		System.out.println("--------------");
+		System.out.print("계좌번호: ");
+		String ano = scanner.next();
+		System.out.print("예금액: ");
+		int money = scanner.nextInt();
+
+		Account account = findAccount(ano);
 		
+		if(account == null) {
+			System.out.println("결과: 계좌가 없습니다.");
+		} else {
+			account.setBalance(account.getBalance()+money);
+			System.out.println("결과: 입금이 성공되었습니다.");
+		}
+		
+
+		
+
 	}
-	
-	//출금하기
+
 	private static void withdraw() {
+		System.out.println("--------------");
+		System.out.println("출금");
+		System.out.println("--------------");
+		System.out.print("계좌번호: ");
+		String ano = scanner.next();
+		System.out.print("출금액: ");
+		int money = scanner.nextInt();
+
+		Account account = findAccount(ano);
 		
-	}	
-	
-	//Account 배열에서 ano와 동일한 Account 객체 찾기
-	private static Account findAccount(String ano) {
-		return null;
-	
+		if(account == null) {
+			System.out.println("결과: 계좌가 없습니다.");
+		} else {
+			if(account.getBalance()>=money) {
+				account.setBalance(account.getBalance()-money);
+				System.out.println("결과: 출금이 성공되었습니다.");
+			} else {
+				System.out.println("잔액이 부족합니다.");
+			}
+		}
 	}
+
+	private static Account findAccount(String ano) {
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				if (accountArray[i].getAno().equals(ano)) {
+					return accountArray[i];
+				}
+			}
+		}
+		return null;
+	}
+
 }
